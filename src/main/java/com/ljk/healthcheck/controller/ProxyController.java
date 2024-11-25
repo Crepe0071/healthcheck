fc5719678041e30f384c997e93bf94b4f8055f9d
sakur35a <sakur35a@naver.com>
2024-11-25 23:50:16 +0900

    feature: add header log and body to buffer

--- src/main/java/com/ljk/healthcheck/controller/ProxyController.java
+++ src/main/java/com/ljk/healthcheck/controller/ProxyController.java
@@ -8,7 +8,6 @@ import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -23,8 +22,8 @@ public class ProxyController {
 
 	private final WebClient webClient;
 
-	@PostMapping("/**")
-	public Mono<ResponseEntity<String>> forwardRequest(ServerHttpRequest request) {
+	@RequestMapping("/**")
+	public Mono<ResponseEntity<DataBuffer>> forwardRequest(ServerHttpRequest request) {
 
 		String url;
 
@@ -36,17 +35,19 @@ public class ProxyController {
 			return Mono.empty();
 		}
 
-		System.out.println("url:: " + url);
+		System.out.println(url + request.getPath().toString().substring(6));
 
 		return webClient.method(request.getMethod())
-				   .uri(url)
+				   .uri(url + request.getPath().toString().substring(6))
 				   .headers(headers -> headers.addAll(request.getHeaders()))
 				   .body(request.getBody(), DataBuffer.class)
 				   .exchangeToMono(clientResponse -> {
 					   HttpHeaders responseHeaders = new HttpHeaders();
+
+					   System.out.println("headers:: " + clientResponse.headers().asHttpHeaders());
 					   clientResponse.headers().asHttpHeaders().forEach(responseHeaders::addAll);
 
-					   return clientResponse.bodyToMono(String.class)
+					   return clientResponse.bodyToMono(DataBuffer.class)
 								  .map(body -> ResponseEntity.status(clientResponse.statusCode())
 												   .headers(responseHeaders)
 												   .body(body));
